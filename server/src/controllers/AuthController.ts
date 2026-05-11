cacb31036d2de99f464eeab0b910021cf7b3a878
Anish Ghimire <pnoob189@gmail.com>
2026-05-11 10:42:30 +0545

    second commit

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -66,7 +66,7 @@ class AuthController {
             res.cookie("token", token, {
                 httpOnly: true,
                 secure: true,
-                sameSite: "lax",
+                sameSite: "none",
                 maxAge: 7 * 24 * 60 * 60 * 1000
             });
 
