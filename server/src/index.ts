64b586cadf63c4d9291aec88a057615a97033ebf
Anish Ghimire <pnoob189@gmail.com>
2026-05-11 09:46:21 +0545

    second commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Adjust this to your frontend's URL
   credentials: true // Adjust this to your frontend's URL
 }));
 
